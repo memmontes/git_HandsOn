6b3be38e40c37627e6d525854a6c369856234c01
memmontes <memolina@ugr.es>
2022-06-13 10:56:14 +0200

    second seq

--- seqClass.py
+++ seqClass.py
@@ -12,6 +12,7 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
+args.seq = args.seq.upper()                 # Note we just added this line
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -21,3 +22,4 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+
